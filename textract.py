01798e91db318d4a457e6e60c82452815fa20226
Anjalydiv <87649127+Anjalydiv@users.noreply.github.com>
2021-08-18 15:34:14 +0530

    Update textract.py

--- textract.py
+++ textract.py
@@ -4,7 +4,8 @@ import boto3
 
 #Class to initialize textract,read file and convert contents to text
 class amazon_textract:
-    def __init__(self,accesskey,secretaccesskey,region):          self.textractclient=boto3.client("textract",aws_access_key_id=accesskey,aws_secret_access_key=secretaccesskey,region_name=region)      
+    def __init__(self,accesskey,secretaccesskey,region):          
+        self.textractclient=boto3.client("textract",aws_access_key_id=accesskey,aws_secret_access_key=secretaccesskey,region_name=region)      
     def analyze_document(self,content):
         response=self.textractclient.detect_document_text(Document={'Bytes':content})
         for blocks in response['Blocks']:
